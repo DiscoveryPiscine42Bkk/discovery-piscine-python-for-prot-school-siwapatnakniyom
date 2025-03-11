c3ebea7316e19a43ecbd6854ec7604f671bbf07b
siwapatnakniyom <nakniyomsiwapat@gmail.com>
2025-03-11 10:02:25 +0700

    Add files via upload

---
+++ cell01/ex1.1/name.py
@@ -0,0 +1,10 @@
+first_name = "willaim"  # Replace with your actual first name
+last_name = "danial"    # Replace with your actual last name
+
+# Concatenate last name and first name
+whole_name = last_name + " " + first_name
+
+# Display the variables followed by a new line
+print(first_name)
+print(last_name)
+print(whole_name)
