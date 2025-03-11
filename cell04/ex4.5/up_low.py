b996b3a528cfa4c61a220bcb6a907dfcafd1b9d7
siwapatnakniyom <nakniyomsiwapat@gmail.com>
2025-03-11 13:07:14 +0700

    Add files via upload

---
+++ cell04/ex4.5/up_low.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Ask the user for a string
+user_input = input("Enter a string: ")
+
+# Swap uppercase letters to lowercase and vice versa
+swapped_string = user_input.swapcase()
+
+# Display the result
+print(f"Swapped case: {swapped_string}")
