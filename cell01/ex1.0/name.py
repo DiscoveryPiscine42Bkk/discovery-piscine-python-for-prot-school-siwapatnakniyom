a776ef14e5eaab7cf839afc57e6f92bf8186b68b
siwapatnakniyom <nakniyomsiwapat@gmail.com>
2025-03-11 09:48:17 +0700

    Add files via upload

---
+++ cell01/ex1.0/name.py
@@ -0,0 +1,5 @@
+first_name = "siwapat"  # Replace with your actual first name
+last_name = "nakniyom"    # Replace with your actual last name
+
+# Display the variables followed by a new line
+print(first_name ,last_name)
