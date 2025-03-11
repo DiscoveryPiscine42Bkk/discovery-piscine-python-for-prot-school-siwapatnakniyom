4cc03716fd4475ec04101eec874d01e9d6ad6781
siwapatnakniyom <nakniyomsiwapat@gmail.com>
2025-03-11 13:30:20 +0700

    Add files via upload

---
+++ cell05/ex5.2/play_with_arrays.py
@@ -0,0 +1,8 @@
+# Original array
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+# Filter values greater than 5
+filtered_array = [num for num in original_array if num > 5]
+
+# Process (for example, print or modify the values)
+print("Filtered values greater than 5:", filtered_array)
