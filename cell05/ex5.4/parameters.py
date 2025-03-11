31ada089341e33d205d63942203164aac40916d2
siwapatnakniyom <nakniyomsiwapat@gmail.com>
2025-03-11 13:39:29 +0700

    Add files via upload

---
+++ cell05/ex5.4/parameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Get the number of arguments (excluding script name)
+num_params = len(sys.argv) - 1
+
+# Print the count followed by a newline
+print(num_params)
