e452499a7bc4e1c6d95bb6e37d4cba488543d234
siwapatnakniyom <nakniyomsiwapat@gmail.com>
2025-03-11 13:13:16 +0700

    Add files via upload

---
+++ cell05/ex5.1/pla_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Define the original array of numbers
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+# Create a new array by adding 2 to each element of the original array
+new_array = [num + 2 for num in original_array]
+
+# Display both arrays
+print("Original array:", original_array)
+print("New array:", new_array)
