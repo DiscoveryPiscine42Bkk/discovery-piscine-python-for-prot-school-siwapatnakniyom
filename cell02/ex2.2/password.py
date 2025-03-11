c6a82f65820858c8292e75e3a65ba31462ba95b9
siwapatnakniyom <nakniyomsiwapat@gmail.com>
2025-03-11 10:58:08 +0700

    Add files via upload

---
+++ cell02/ex2.2/password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Define the correct password
+password = "91399139"
+
+# Prompt the user to enter a password
+user_input = input("Enter the password: ")
+
+# Check if the entered password is correct
+if user_input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
